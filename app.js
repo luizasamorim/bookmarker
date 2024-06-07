40557c342d381db47069383b4f4b3af7e43b4660
luizasamorim <luizassamorim@gmail.com>
2024-06-07 06:22:08 -0300

    dotenv

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const express = require('express')
+const express = require("express")
+require("dotenv").config()
 
-const bookmarkRouter = require('./routes/bookmark')
+const bookmarkRouter = require("./routes/bookmark")
+const installRouter = require("./routes/install")
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// app.use('/bookmark', bookmarkRouter)
+app.use("/install", installRouter)
+app.use("/bookmarks", bookmarkRouter)
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('teste')
-})
+// const {create} = require("./models/Bookmark")
+// app.get("/", async (req, res) => {
+//   try {
+//     let response = await create(1,"aaa","aaa","aaa","aaa")
+//     console.log(response)
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error)
+//   }
+//   res.send("foi")
+// })
 
 app.listen(port, () => {
   console.log(`Rodando em http://localhost:${port}`)
